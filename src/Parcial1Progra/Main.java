a43ec92a9dd945902a53ec340a65da88bdcfa6a0
OscarZacarias0106 <ozacariaso@miumg.edu.gt>
2021-08-08 17:35:17 -0600

    ProyectoCalculadora

---
+++ src/Parcial1Progra/Main.java
@@ -0,0 +1,21 @@
+package Parcial1Progra;
+
+
+import java.util.Scanner;
+
+class main {
+
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        //Indica si salimos o no
+        boolean salir = false;
+        int opcion;
+
+        //Bucle para pedir las opciones hasta que elijamos salir
+        while (!salir) {
+
+        }
+    }
+}
