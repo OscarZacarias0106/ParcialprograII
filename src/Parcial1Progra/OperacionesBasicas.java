4e1c5555f9cb60b4e3726981fb34bd3542e27d75
OscarZacarias0106 <ozacariaso@miumg.edu.gt>
2021-08-08 18:52:16 -0600

    creacion de metodo resta

--- src/Parcial1Progra/OperacionesBasicas.java
+++ src/Parcial1Progra/OperacionesBasicas.java
@@ -33,4 +33,19 @@ public class OperacionesBasicas {
         System.out.println(" ");
 
     }
+    static void multiplicar(){
+        System.out.println("Multiplicacion");
+        Scanner leer = new Scanner(System.in);
+        int num1;
+        int num2;
+        int resultado;
+        System.out.println("Escriba el primer Numero");
+        num1=leer.nextInt();
+        System.out.println("Escriba el segundo numero");
+        num2=leer.nextInt();
+        resultado=num1*num2;
+        System.out.println("La multiplicacion es "+resultado);
+        System.out.println(" ");
+
+    }
 }
