e653cd2d7a7f63e89df8308743c78ba6536a9a24
OscarZacarias0106 <ozacariaso@miumg.edu.gt>
2021-08-08 18:29:12 -0600

    creacion de metodo resta

--- src/Parcial1Progra/OperacionesBasicas.java
+++ src/Parcial1Progra/OperacionesBasicas.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class OperacionesBasicas {
     static void suma(){
-        System.out.println("SUMA");
+        System.out.println("Suma");
         Scanner leer = new Scanner(System.in);
         int num1;
         int num2;
@@ -15,6 +15,21 @@ public class OperacionesBasicas {
         num2=leer.nextInt();
         resultado=num1+num2;
         System.out.println("La suma es "+resultado);
+        System.out.println(" ");
+
+    }
+    static void resta(){
+        System.out.println("Resta");
+        Scanner leer = new Scanner(System.in);
+        int num1;
+        int num2;
+        int resultado;
+        System.out.println("Escriba el primer Numero");
+        num1=leer.nextInt();
+        System.out.println("Escriba el segundo numero");
+        num2=leer.nextInt();
+        resultado=num1-num2;
+        System.out.println("La resta es "+resultado);
         System.out.println(" ");
 
     }
