822fa87c5664c1f3dd80d45f33828d7ecb7006fc
OscarZacarias0106 <ozacariaso@miumg.edu.gt>
2021-08-08 19:38:12 -0600

    terminacion metodo menu de operaciones basicas

--- src/Parcial1Progra/Main.java
+++ src/Parcial1Progra/Main.java
@@ -4,6 +4,8 @@ package Parcial1Progra;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static Parcial1Progra.OperacionesBasicas.*;
+
 class main {
 
 
@@ -61,6 +63,55 @@ class main {
         int opcion;
 
         //Bucle para pedir las opciones hasta que elijamos salir
-        while (!salir);
+        while (!salir){
+
+            //opciones
+            System.out.println("--Bienvendio a caluladora Basica--  ");
+            System.out.println("1. suma            1");
+            System.out.println("2. resta           2");
+            System.out.println("3. multiplicacion  3");
+            System.out.println("4. division        4");
+            System.out.println("5. Regresar al menu Principal");
+
+
+            try{
+
+                //Pide una opcion
+                System.out.println("Introduce un numero: ");
+                opcion = sn.nextInt();
+
+                //Realiza una de las opciones
+                switch (opcion) {
+                    case 1:
+                        suma();
+                        break;
+                    case 2:
+                        resta();
+                        break;
+                    case 3:
+                        multiplicar();
+                        break;
+                    case 4:
+                        division();
+                        break;
+                    case 5:
+                        salir=true;
+                        break;
+                    default:
+                        System.out.println("Las opciones son entre 1 y 5");
+                }
+
+
+                //controla la excepcionn en caso de que se introduzca un valor no correcto
+            }catch(InputMismatchException e){
+                System.out.println("Debes escribir un numero");
+                sn.next();
+            }
+
+        }
+
+        System.out.println("Fin del menu");
+        }
     }
+
 }
